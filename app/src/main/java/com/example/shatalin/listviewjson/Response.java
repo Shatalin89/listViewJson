7f7cff2506e5f89109d348f7c7ef3ff67f34f5d2
Shatalin89 <Shatalin89@yandex.ru>
2016-11-11 00:49:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shatalin/listviewjson/Response.java
@@ -0,0 +1,17 @@
+package com.example.shatalin.listviewjson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Shatalin on 10.11.2016.
+ */
+
+public class Response {
+
+    private List<AndroidVersion> android = new ArrayList<AndroidVersion>();
+
+    public List<AndroidVersion> getAndroid() {
+        return android;
+    }
+}
